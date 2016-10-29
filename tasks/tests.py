6f36d9613db8202255566d5a60bc624039603fa4
Thiago Chagas <tqchagas@gmail.com>
2016-10-29 14:14:06 -0200

    add login test

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,38 @@
+from datetime import datetime, timedelta
+
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APIClient
+
+
+class BaseTestCase(TestCase):
+
+    def setUp(self):
+        self.email = 'usario@usuario.com'
+        self.username = 'usuario'
+        self.password = '12345'
+        self.user = User.objects.create_user(
+            self.username, self.email, self.password)
+
+        self.data = {
+            'username': self.username,
+            'password': self.password
+        } 
+
+    def get_token(self):
+        client = APIClient(enforce_csrf_checks=True)
+        response = client.post('/auth/', self.data, format='json')
+        self.assertEqual(response.status_code, 200)
+        return response.data.get('token')
+
+
+class LoginTestCase(BaseTestCase):
+    def setUp(self):
+        return super(LoginTestCase, self).setUp()
+    
+    def test_login(self):
+        client = APIClient(enforce_csrf_checks=True)
+        response = client.post('/auth/', self.data, format='json')
+        token = self.get_token()
+        self.assertEqual(response.status_code, 200)
