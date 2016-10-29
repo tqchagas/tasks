75cbb062a04932291cda89846ca29b43c2eb1c85
Thiago Chagas <tqchagas@gmail.com>
2016-10-29 15:25:35 -0200

    validação inicio/termino

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,10 +4,9 @@ from tasks.models import Tarefa
 from rest_framework import serializers
 
 
-
 class TarefaSerializer(serializers.ModelSerializer):
-    class Meta:
 
+    class Meta:
         model = Tarefa
 
         fields = (
@@ -17,3 +16,13 @@ class TarefaSerializer(serializers.ModelSerializer):
             'descricao',
             'encerrada'
         )
+
+    def validate(self, data):
+        inicio = data.get('inicio')
+        termino = data.get('termino')
+        if inicio > termino:
+            msg = u'Data de início deve ser menor ou igual a data de término.'
+            raise serializers.ValidationError({
+                'erro': msg
+            })
+        return data
